f87cf7990a0170e31282214bc3a9ed9431dff05e
andrewvitrenko <avitrenko2004@gmail.com>
2023-03-11 17:16:24 +0100

    feat: added example of using each method in list

--- index.js
+++ index.js
@@ -63,3 +63,27 @@ class LinkedList {
     this.data = [...this.data, ...list];
   }
 }
+
+const linkedList = new LinkedList();
+
+console.log(linkedList.length()); // output: 0
+linkedList.extend(['1', '2', '3', '4', '1', '3', '3', '3']);
+console.log(linkedList.length()); // output: 8
+console.log(linkedList.get(7)); // output: 3
+linkedList.append('5');
+console.log(linkedList.length()); // output: 9
+console.log(linkedList.get(8)); // output: 5
+linkedList.insert('6', 2);
+console.log(linkedList.length()); // output: 10
+console.log(linkedList.get(2)); // output: 6
+console.log(linkedList.findFirst('2')); // output: 1
+console.log(linkedList.findLast('3')); // output: 8
+console.log(linkedList.clone()); // output: ['1', '2', '6', '3', '4', '1', '3', '3', '3', '5']
+linkedList.reverse();
+console.log(linkedList.clone()); // output: ['5', '3', '3', '3', '1', '4', '3', '6', '2', '1']
+console.log(linkedList.delete(2)); // output: 3
+console.log(linkedList.length()); // output: 9
+linkedList.deleteAll('3');
+console.log(linkedList.length()); // output: 6
+linkedList.clear();
+console.log(linkedList.length()); // output: 0
